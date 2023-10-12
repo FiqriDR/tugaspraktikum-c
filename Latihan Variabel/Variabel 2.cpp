b26f514c7b95ec608a25d33908f785c05fb5747d
FiqriDR <147030008+FiqriDR@users.noreply.github.com>
2023-10-12 14:28:16 +0700

    Add files via upload

---
+++ Latihan Variabel/Variabel 2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	char nim[10];
+	char nama[30];
+	int nilai;
+	
+	nim = "2143007";
+	nama = "Fiqri Dwiyan Rizaldy";
+	nilai = 85;
+	
+	printf("NIM : %s", nim);
+	printf("NAMA : %s", nama);
+	printf("NILAI : %i", nilai);
+	
+	return 0;
+}
---
+++ Tugas Mandiri pertemuan 1/Pertemuan 2/Menghitung luas segitiga.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int alas = 8;
+	int tinggi = 5;
+	int hasil = 0.5 * alas * tinggi;
+	printf("Hasil dari luas segitiga adalah %d", hasil);
+	
+	return 0;
+} 
